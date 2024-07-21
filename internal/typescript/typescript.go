7cfbdcf85453a5d63377d6e16c86d1f296388967
Jacob Hummer <jcbhmr@outlook.com>
2024-07-21 20:36:01 +0000

    saeve

--- internal/typescript/typescript.go
+++ internal/typescript/typescript.go
@@ -1,6 +1,10 @@
 package typescript
 
-import _ "embed"
+import (
+	_ "embed"
+)
 
+//go:generate npm pack typescript@5.5.3
+//go:generate mv typescript-5.5.3.tgz typescript.tgz
 //go:embed typescript.tgz
 var TypescriptTGZ []byte
